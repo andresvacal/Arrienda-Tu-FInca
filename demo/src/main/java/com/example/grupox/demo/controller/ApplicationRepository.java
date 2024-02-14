10cb297a3b43c0bb462a6416da5f036e56437958
andresvacal <adbes07@gmail.com>
2024-02-14 11:03:59 -0500

    Configuración con base de datos

---
+++ demo/src/main/java/com/example/grupox/demo/controller/ApplicationRepository.java
@@ -0,0 +1,11 @@
+package com.example.grupox.demo.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.grupox.demo.entity.Application;
+
+
+public interface ApplicationRepository extends CrudRepository<Application, Long>{
+
+}
---
+++ demo/src/main/java/com/example/grupox/demo/entity/Application.java
@@ -0,0 +1,33 @@
+package com.example.grupox.demo.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Where(clause = "status = 0")
+@SQLDelete(sql = "UPDATE application SET  status = 1 WHERE id=?")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String url;
+    private String code;
+    private String internalCode;
+
+    
+}
