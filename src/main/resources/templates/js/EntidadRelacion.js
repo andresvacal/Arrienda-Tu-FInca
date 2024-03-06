49d44943138ada4408720509cb7349a15e110684
LondoFeli <felilondoo@outlook.es>
2024-03-05 19:27:58 -0500

    JS de entidad relacion

---
+++ src/main/resources/templates/js/EntidadRelacion.js
@@ -0,0 +1,8 @@
+var BotonAtras = document.getElementById("BotonAtras")
+var BotonSiguiente = document.getElementById("BotonSiguiente")
+var BotonHome = document.getElementById("BotonHome")
+BotonSiguiente.onclick(window.location.href='/grupo27/endpoints')
+BotonAtras.onclick(window.location.href='/grupo27/diagrama/componentes')
+BotonHome.onclick(window.location.href='/grupo27/homescreen')
+
+
