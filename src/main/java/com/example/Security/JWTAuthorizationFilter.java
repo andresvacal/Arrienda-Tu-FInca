1799a5c064395aae460fc7ca707365a03a5ebc85
LondoFeli <felilondoo@outlook.es>
2024-06-03 00:25:25 -0500

    pij0i
    0ij0ih

--- src/main/java/com/example/Security/JWTAuthorizationFilter.java
+++ src/main/java/com/example/Security/JWTAuthorizationFilter.java
@@ -24,53 +24,75 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Component
 @Service
-public class JWTAuthorizationFilter extends OncePerRequestFilter{
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
     public static final String HEADER = "Authorization";
-	public static final String PREFIX = "Bearer ";
+    public static final String PREFIX = "Bearer ";
+
     @Autowired
-	private CustomUserDetailsService userDetailsService;
+    private CustomUserDetailsService userDetailsService;
+
     @Autowired
     private JWTTokenService jwtTokenService;
-	@Override
-	protected void doFilterInternal( @NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		try {
-			if (existeJWTToken(request)) {
-				Claims claims = validarToken(request);
-				if (claims.get("authorities") != null) {
-					String username = getUsername(request);
-					UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-					UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, userDetails, null);
-					auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-           			SecurityContextHolder.getContext().setAuthentication(auth);
-				} else {
-					SecurityContextHolder.clearContext();
-				}
-			} else {
-				SecurityContextHolder.clearContext();
-			}
-			chain.doFilter(request, response);
-		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
-		}
-	}	
 
-	private Claims validarToken(HttpServletRequest request) {
-		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
-		return jwtTokenService.decodificarToken(jwtToken);
-	}
-	private String getUsername(HttpServletRequest request) {
-		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
-		return jwtTokenService.getUsername(jwtToken);
-	}
-	private boolean existeJWTToken(HttpServletRequest request) {
-		String authenticationHeader = request.getHeader(HEADER);
-		return !(authenticationHeader == null || !authenticationHeader.startsWith(PREFIX));
-	}
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
+        System.out.println("-------->>>-------->>> Filtro");
+        try {
+            if (existeJWTToken(request)) {
+                Claims claims = validarToken(request);
+                if (claims.get("authorities") != null) {
+                    String username = getUsername(request);
+                    System.out.println("Usuario autenticado: " + username);
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    if (jwtTokenService.validateToken(request.getHeader(HEADER).replace(PREFIX, ""), userDetails.getUsername())) {
+                        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+                        auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(auth);
+                        System.out.println("Autenticación exitosa para el usuario: " + username);
+                    } else {
+                        System.out.println("Token inválido para el usuario: " + username);
+                    }
+                } else {
+                    SecurityContextHolder.clearContext();
+                    System.out.println("El token no contiene las autoridades necesarias.");
+                }
+            } else {
+                SecurityContextHolder.clearContext();
+                System.out.println("No se encontró un token JWT en la solicitud.");
+            }
+            chain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            System.out.println("El token JWT ha expirado: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "El token JWT ha expirado.");
+        } catch (UnsupportedJwtException e) {
+            System.out.println("El token JWT no es soportado: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "El token JWT no es soportado.");
+        } catch (MalformedJwtException e) {
+            System.out.println("El token JWT está malformado: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "El token JWT está malformado.");
+        } catch (SignatureException e) {
+            System.out.println("La firma del token JWT no es válida: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "La firma del token JWT no es válida.");
+        } catch (Exception e) {
+            System.out.println("Error al validar el token JWT: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Error al validar el token JWT.");
+        }
+    }
+
+    private Claims validarToken(HttpServletRequest request) {
+        String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+        return jwtTokenService.decodificarToken(jwtToken);
+    }
+
+    private String getUsername(HttpServletRequest request) {
+        String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+        return jwtTokenService.getUsername(jwtToken);
+    }
+
+    private boolean existeJWTToken(HttpServletRequest request) {
+        String authenticationHeader = request.getHeader(HEADER);
+        return authenticationHeader != null && authenticationHeader.startsWith(PREFIX);
+    }
 }
--- src/main/java/com/example/Service/JWTTokenService.java
+++ src/main/java/com/example/Service/JWTTokenService.java
@@ -16,49 +16,46 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JWTTokenService {
-// @Value("${jwt.secret}")
-    // private String secret = "DES6123";
-    // @Value("${jwt.expiration}")
     private long jwtExpiration = 99999999;
-    private Key jwtKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);; // You need to set this key appropriately
+    private Key jwtKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
     public String generarToken(ArrendadorDTO usuario) {
-        // byte[] secretBytes = secret.getBytes();
-        // Key jwtKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS512.getJcaName());
-        ObjectMapper objectMapper = new ObjectMapper();
-        String username = "";
-        try {
-            username = objectMapper.writeValueAsString(usuario);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(username  );
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpiration);
         Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(usuario.getCorreoElectronico()) // Almacenar solo el correo electrónico como sujeto
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .claim("authorities", authorities.stream()
                         .map(GrantedAuthority::getAuthority)
                         .collect(Collectors.toList()))
-                .signWith(jwtKey, SignatureAlgorithm.HS512) // Use your appropriate signing algorithm
+                .signWith(jwtKey, SignatureAlgorithm.HS512)
                 .compact();
     }
-    public String getUsername(String jwtToken){
+
+    public String getUsername(String jwtToken) {
         return decodificarToken(jwtToken).getSubject();
     }
 
-    public Date getFechaExpiracion(String jwtToken){
+    public Date getFechaExpiracion(String jwtToken) {
         return decodificarToken(jwtToken).getExpiration();
     }
+
     public Claims decodificarToken(String jwtToken) {
-        // byte[] secretBytes = secret.getBytes();
-        // Key jwtKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS512.getJcaName());
         return Jwts.parserBuilder()
-                            .setSigningKey(jwtKey)
-                            .build()
-                            .parseClaimsJws(jwtToken)
-                            .getBody();
+                .setSigningKey(jwtKey)
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    public boolean validateToken(String token, String username) {
+        final String tokenUsername = getUsername(token);
+        return (tokenUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getFechaExpiracion(token).before(new Date());
     }
 }
